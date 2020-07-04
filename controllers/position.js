bece8ddfe86fff8f5a68be08bc94eb261d9ca14a
Faridun <madysoftware@gmail.com>
2020-07-04 10:59:08 +0500

    Готовый контроллер для Position и Функционал для загрузки файлов(кртинок: png, jpeg)

--- controllers/position.js
+++ controllers/position.js
@@ -1,15 +1,51 @@
-module.exports.read = function (req, res) {
+const Position = require('../models/Position');
+const errorHandler = require('../utils/errorHandler');
 
+module.exports.read = async function (req, res) {
+    try {
+        const position = await Position.find({
+            category: req.params.categoryId,
+            user: req.user.id
+        });
+        res.status(200).json(position);
+    } catch (e) {
+        errorHandler(res, e);
+    }
 }
 
-module.exports.create = function (req, res) {
+module.exports.create = async function (req, res) {
+    try {
+        const position = await new Position({
+            name: req.body.name,
+            cost: req.body.cost,
+            category: req.body.category,
+            user: req.user.id
+        }).save();
 
+    } catch (e) {
+        errorHandler(res, e);
+    }
 }
 
-module.exports.delete = function (req, res) {
-
+module.exports.delete = async function (req, res) {
+    try {
+        await Position.remove({ id: req.params.id });
+        res.status(200).json({
+            message: 'Позиция удалена успешно!'
+        })
+    } catch (e) {
+        errorHandler(res, e);
+    }
 }
 
-module.exports.update = function (req, res) {
-
+module.exports.update = async function (req, res) {
+    try {
+        const position = await Position.findOneAndUpdate(
+            { id: req.params.id },
+            { $set: req.body },
+            { new: true });
+        res.status(200).json(position);
+    } catch (e) {
+        errorHandler(res, e);
+    }
 }
